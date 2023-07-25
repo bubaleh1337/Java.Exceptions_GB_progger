b4f2283c841270e18beabeed39e2d3ba17a181ae
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-07-25 14:16:12 +0500

    was added solution for task2 of hw2 sem2

--- sem2/hw2/task2.java
+++ sem2/hw2/task2.java
@@ -1,5 +1,26 @@
 package sem2.hw2;
 
-public class task2 {
+import java.util.Scanner;
 
+/*
+Разработайте программу, которая выбросит Exception,
+когда пользователь вводит пустую строку. Пользователю
+должно показаться сообщение, что пустые строки вводить нельзя.
+ */
+public class task2 {
+    public static void main(String[] args) {
+        prompt();
+    }
+    public static void prompt() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите не пустую строку: ");
+        String line = scanner.nextLine();
+        if(!line.isEmpty()) {
+            System.out.println(line);
+            prompt();
+        }
+        else {
+            throw new RuntimeException("Пустые строки вводить нельзя.");
+        }
+    }
 }
